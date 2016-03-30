31dc2e40973a1200078ec19f5e6025b79697b91d
Eduardo Lopez <edu230895@gmail.com>
2016-03-30 12:08:48 +0200

    Alter Table modificado con los nuevos datos de jobOffers

--- database/migrations/2016_03_27_112116_AlterJobOffers.php
+++ database/migrations/2016_03_27_112116_AlterJobOffers.php
@@ -14,6 +14,7 @@ class AlterJobOffers extends Migration
     {
         Schema::table('jobOffers', function (Blueprint $table) {
             $table->foreign('enterpriseResponsable_id')->references('id')->on('enterpriseResponsables')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('profFamilie_id')->references('id')->on('profFamilies')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -25,7 +26,8 @@ class AlterJobOffers extends Migration
     public function down()
     {
         Schema::table('jobOffers', function (Blueprint $table) {
-            $table->dropForeign('joboffers_enterpriseResponsable_id_foreign');
+            $table->dropForeign('joboffers_enterpriseresponsable_id_foreign');
+            $table->dropForeign('joboffers_proffamilie_id_foreign');
         });
     }
 }
