b35668cc455efa15cba7e22170bb5ee48b467a2f
Sikay <pedrohdezmora@gmail.com>
2016-04-01 11:22:35 +0200

    Procedimiento ofertas de trabajo y Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            \DB::select("call fechaExpiracion(" . date('Ymd') . ")");
+        })->daily();
+
     }
 }
