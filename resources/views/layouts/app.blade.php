ca0c8e42ad7f61d66f1efb853938f3cd7df19136
Abel <monteabel@gmail.com>
2016-04-02 12:30:19 +0200

    Prueba Nueva Rama

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,9 +70,10 @@
                         </li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <img src="cinqueterre.jpg" class="img-rounded dropdown-toggle" alt="Cinque Terre" data-toggle="dropdown" role="button" width="50" height="50">
+                           <!--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->email }} <span class="caret"></span>
-                            </a>
+                            </a>-->
 
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
