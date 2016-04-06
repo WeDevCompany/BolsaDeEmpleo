87ef0ba714e66eb19a2626f0056c9a21080c5783
khru <evrtrabajo@gmail.com>
2016-04-06 21:19:53 +0200

    Configuración de mailgun, subire el .env con la configuración a drive

--- resources/views/partials/parallax/parallaxWelcome.blade.php
+++ resources/views/partials/parallax/parallaxWelcome.blade.php
@@ -1,19 +1,19 @@
-    <!-- Parallax effect -->
-        <section class="parallax" id="parallax-1">
-            <div class="container text-center">
-            <i class="fa fa-cog fa-spin fa-5x fa-fw margin-bottom"></i>
-                <div class="carousel-caption">
+<!-- Parallax effect -->
+<section class="parallax" id="parallax-1">
+    <div class="container text-center">
+    <i class="fa fa-cog fa-spin fa-5x fa-fw margin-bottom"></i>
+        <div class="carousel-caption">
 
-                        <div class="verticalcenter white">
+                <div class="verticalcenter white">
 
-                            <div class="animated fadeInDown">
+                    <div class="animated fadeInDown">
 
-                                <h4>Si eres una empresa y buscas trabajadores</h4>
-                                <h5>Si necesitas trabajadores cualificados y con altos conocimientos en su área de especialización no dude en registrarse</h5>
-                                <a href="/empresa" target="_blank" class="btn btn-default btn-stc waves-effect waves-light"><i class="fa fa-building right"></i>Empresa</a>
-                            </div>
-                        </div>
+                        <h4>Si eres una empresa y buscas trabajadores</h4>
+                        <h5>Si necesitas trabajadores cualificados y con altos conocimientos en su área de especialización no dude en registrarse</h5>
+                        <a href="/empresa" target="_blank" class="btn btn-default btn-stc waves-effect waves-light"><i class="fa fa-building right"></i>Empresa</a>
                     </div>
+                </div>
             </div>
-        </section>
-    <!-- / .Parallax -->
+    </div>
+</section>
+<!-- / .Parallax -->
