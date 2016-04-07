25d3d99b586bfd5d11fe4d259a57930feb926558
Sikay <pedrohdezmora@gmail.com>
2016-04-07 22:10:47 +0200

    Login reCapcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,16 @@
                                         </span>
                                     @endif
                                 </div>
+                                <div class="text-center{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
+                                    {!! app('captcha')->display(); !!}
+                                </div>
+                                <div class="text-center">
+                                    @if ($errors->has('g-recaptcha-response'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                                 <div class="text-center">
                                     <button type="submit" class="btn btn-primary waves-effect waves-light btn-login-media">
                                         <i class="fa fa-sign-in"></i>
