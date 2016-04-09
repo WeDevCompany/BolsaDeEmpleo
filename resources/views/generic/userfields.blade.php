1c171f1dfc1fe5a741fe8ade6095645276c2435f
Abel <monteabel@gmail.com>
2016-04-09 14:23:23 +0200

    Modificado userfields y actualizado form_blade teacher

--- resources/views/generic/userfields.blade.php
+++ resources/views/generic/userfields.blade.php
@@ -1,6 +1,11 @@
-    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">    
+        <div class="row">
+            <div class="input-field col-md-12">
+            <i class="material-icons prefix">email</i>
         {{ Form::label('email', 'Correo electrónico') }}
-        {{ Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'E-mail de usuario']) }}
+        {{ Form::text('email', null) }}
+                </div>
+    </div>
         @if ($errors->has('email'))
             <span class="help-block">
                 <strong>{{ $errors->first('email') }}</strong>
@@ -8,8 +13,13 @@
         @endif
     </div>
     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+        <div class="row">
+        <div class="input-field col-md-12">
+            <i class="material-icons prefix">lock</i>
         {{ Form::label('password', 'Contraseña') }}
-        {{ Form::text('password', null, ['class' => 'form-control', 'placeholder' => 'Contraseña']) }}
+        {{ Form::text('password', null) }}
+                </div>
+    </div>
         @if ($errors->has('password'))
             <span class="help-block">
                 <strong>{{ $errors->first('password') }}</strong>
@@ -17,8 +27,13 @@
         @endif
     </div>
     <div class="form-group{{ $errors->has('password2') ? ' has-error' : '' }}">
+       <div class="row">
+        <div class="input-field col-md-12">
+            <i class="material-icons prefix">lock</i>
         {{ Form::label('password2', 'Confirmar contraseña') }}
-        {{ Form::text('password2', null, ['class' => 'form-control', 'placeholder' => 'Repetición de contraseña']) }}
+        {{ Form::text('password2', null) }}
+                </div>
+    </div>
         @if ($errors->has('password2'))
             <span class="help-block">
                 <strong>{{ $errors->first('password2') }}</strong>
--- resources/views/teacher/form.blade.php
+++ resources/views/teacher/form.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 
 @section('content')
@@ -7,22 +8,102 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Bienvenido Profesor</div>
+
                 <div class="panel-body">
-                    @if(Session::has('message_Positive'))
-                        <p class="alert alert-success">
-                        {{ Session::get('message_Positive') }}
-                        </p>
-                    @elseif(Session::has('message_Negative'))
-                        <p class="alert" style="color:black;background-color:#ffacac;border-color:#a94442">
-                        {{ Session::get('message_Negative') }}
-                        </p>
-                    @endif
-                    {{ Form::open(['route' => 'profesor..store', 'method' => 'POST', 'action' => '/profesor']) }}
-                    @include('generic.userfields')
-                    @include('teacher.teacherfields')
-                    <button type="submit" class="btn btn-default">
-                        Darme de alta
-                    </button>
+                    <p>Formulario de registro para PROFESORES</p>
+                     {{ Form::open(['route' => 'profesor..store', 'method' => 'POST', 'files' => 'true']) }}
+                        {!! csrf_field() !!}
+                        <fieldset>
+                        <legend style="width:auto;">Profesor</legend>
+
+                        <div class="control-group{{ $errors->has('firstName') ? ' has-error' : '' }}">
+                            <div class="row">
+                                <div class="input-field col-md-12">
+                                    <i class="material-icons prefix">account_circle</i>
+                                    {{ Form::text('firstName', null) }}
+                                    {{ Form::label('firstName', 'Nombre') }}
+
+                                </div>
+                            </div>
+                                @if ($errors->has('firstName'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('firstName') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+
+                        <div class="control-group{{ $errors->has('lastName') ? ' has-error' : '' }}">
+                            <div class="row">
+                                <div class="input-field col-md-12">
+                                    <i class="material-icons prefix">account_circle</i>
+                                    {{ Form::text('lastName', null) }}
+                                    <label for="lastName">Apellidos</label>
+                                </div>
+                            </div>
+                                @if ($errors->has('lastName'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('lastName') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+
+                        <div class="control-group{{ $errors->has('dni') ? ' has-error' : '' }}">
+                            <div class="row">
+                                <div class="input-field col-md-12">
+                                    <i class="material-icons prefix">assignment_ind</i>
+                                    {{ Form::text('dni', null) }}
+                                    <label for="dni">DNI</label>
+                                </div>
+                            </div>
+                                @if ($errors->has('dni'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('dni') }}</strong>
+                                    </span>
+                                @endif
+                        </div>                        
+
+                        <div class="control-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <div class="row">
+                                <div class="input-field col-md-12">
+                                    <i class="material-icons prefix">phone</i>
+                                    {{ Form::text('phone', null) }}
+                                    <label for="phone">Teléfono</label>
+                                </div>
+                            </div>
+                                @if ($errors->has('phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                        </div>                        
+                        
+                        <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
+                        {{ Form::label('image', 'Subir Imágen', ['class' => 'col-md-12']) }}
+                          
+                            <div class="col-md-6">
+                            {{ Form::file('image', null, ['class' => 'form-control']) }}
+
+                                @if ($errors->has('image'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('image') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        </fieldset>
+                       
+
+                        <fieldset>
+                            <legend style="width: auto;">Usuario</legend>
+                            @include('generic.userfields')
+                        </fieldset>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-user"></i>Registrar
+                                </button>
+                            </div>
+                        </div>
                     {{ Form::close() }}
                 </div>
             </div>
@@ -30,6 +111,4 @@
     </div>
 </div>
 @endsection
-
-
                 
