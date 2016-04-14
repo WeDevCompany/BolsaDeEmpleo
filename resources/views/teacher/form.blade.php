500d73be39341caa2db47db0969e5c283b144947
Sikay <pedrohdezmora@gmail.com>
2016-04-14 19:58:37 +0200

    Drag and drop

--- resources/views/teacher/form.blade.php
+++ resources/views/teacher/form.blade.php
@@ -77,18 +77,35 @@
                                 @endif
                         </div>                        
                         
-                        <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
-                        {{ Form::label('image', 'Subir Imágen', ['class' => 'col-md-12']) }}
+                        <div class="form-group{{ $errors->has('file') ? ' has-error' : '' }}">
                           
-                            <div class="col-md-6">
-                            {{ Form::file('image', null, ['class' => 'form-control']) }}
+                            <div class="col-md-12">
+                            
+                            <!-- Drag and Drop -->
+                            <a id="file-select">
+                                <div class="drop" id="drop">
+                                        <img id="show" src="">
+                                        <div class="text-center">
+                                            <span>Drag and Drop or Click</span>
+                                        </div>
+                                        
+                                </div>
+                            <a>
+                            <!-- FIN Drag and Drop -->
 
-                                @if ($errors->has('image'))
+                            <span class="alert alert-info" id="file-info">No hay archivo aún</span>
+                            {{ Form::file('file', null, ['id' => 'file']) }}
+
+                                @if ($errors->has('file'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('image') }}</strong>
+                                        <strong>{{ $errors->first('file') }}</strong>
                                     </span>
                                 @endif
                             </div>
+                        </div>
+                        <div class="form-control">
+                        {{ Form::select('select',array('L' => 'Large', 'S' => 'Small'), null,['class' => 'chosen-select form-control', 'multiple']) }}
+
                         </div>
                         </fieldset>
                        
@@ -110,5 +127,88 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+
+<script>
+
+    // Indicamos cual sera el campo select multiple
+    $('.chosen-select').chosen([]);
+
+    // Ocultar campos al iniciar el documento
+    $(document).on('ready', function(){
+        $('input[type=file]').hide();
+        $('#file-info').hide();
+        $('.drop img').hide();
+
+    });
+
+    // Funcion para mostrar la imagen
+    $('input[type=file]').change(function() {
+
+        // Nombre archivo usuario
+        var file = (this.files[0].name).toString();
+        var reader = new FileReader();
+        
+        // Vaciamos el contenido y añadimos el nuevo donde mostraremos el nombre del archivo
+        $('#file-info').text('');
+        $('#file-info').text(file);
+        
+        reader.onload = function (e) {
+
+            // Mostrar imagen
+            $('.drop img').attr('src', e.target.result);
+
+            $('.drop img').show();
+            $('#file-info').show();
+            $('.drop span').hide();
+
+        }
+         
+        reader.readAsDataURL(this.files[0]); 
+
+    });
+
+    // Funcion para drag and drop
+    $('.drop').on("dragover drop", function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+    }).on("drop", function(e) {
+
+            // objeto FileList
+            var files = e.originalEvent.dataTransfer.files;
+            var file = files[0];
+
+            var metadata = [];
+
+            // Comprobamos que es una imagen
+            if (file.type.match('image.*')) {
+
+                // "Introducimos" la imagen en el input file
+                $("input[type='file']").prop("files", e.originalEvent.dataTransfer.files);
+
+                $(this).css("border", "2px solid green");
+
+            } else {
+
+                $(this).css("border", "2px solid red");
+
+                // error
+
+            }
+
+    });
+
+    // Al hacer click en el drag and drop se abre la ventana de subida de archivos
+    $('.drop').on('click', function(e) {
+        
+        $("input[type='file']").click();
+
+    })
+
+</script>
+
 @endsection
                 
