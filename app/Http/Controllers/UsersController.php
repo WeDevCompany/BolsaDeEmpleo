b7ede29f3693826f6878c545167eaf0f0ff75b6f
Sikay <pedrohdezmora@gmail.com>
2016-04-19 17:48:32 +0200

    Modificado UploadImages-> Añadido dropzone y css. Mejora de request en UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -67,15 +67,9 @@ class UsersController extends Controller
 
         // Valido la peticion.
         $this->validate($this->request, $this->rules);
-
-        // Obtengo el array con los datos de la peticion.
-        $req = array_map('trim', $this->request->all());
-
+        
         // Añado el rol.
-        $req['rol'] = $this->rol;
-
-        // Remplazo en la peticion los cambios para añadir el rol.
-        $this->request->replace($req);
+        $this->request['rol'] = $this->rol;
 
         // Creo el usuario y devuelvo los datos de la insercion.
         $user = Self::create();
@@ -83,10 +77,8 @@ class UsersController extends Controller
         if($user === false){
             return false;
         } else {
-            $req['user_id'] = $user['id'];
 
-            // Remplazo en la peticion los cambios para añadir el user_id.
-            $this->request->replace($req);
+            $this->request['user_id'] = $user['id'];
 
             // Devuelvo los datos de la insercion.
             return $user;
@@ -111,38 +103,43 @@ class UsersController extends Controller
             // Tratamiento de la imagen
             if (!empty($this->request->file('file'))) {
 
+                // Imagen recibida si existe
                 $file = $this->request->file('file');
                 $imagen = $file->getClientOriginalName();
 
             } else {
+
+                // Imagen por defecto aleatoria
                 $imagen = $faker->randomElement(['default_1.png', 'default_2.png',
                  'default_3.png', 'default_4.png', 'default_5.png',
                  'default_6.png', 'default_7.png', 'default_8.png',
                  'default_9.png', 'default_10.png', 'default_11.png']);
             }
 
+            // Añadimos la imagen para insertarla
             $this->request['image'] = $imagen;
 
-            // Obtengo el array con los datos de la peticion.
-            $req = array_map('trim', $this->request->all());
-
             // Añado la carpeta.
-            $req['carpeta'] = $carpeta;
+            $this->request['carpeta'] = $carpeta;
 
-            // Remplazo en la peticion los cambios para añadir el rol.
-            $this->request->replace($req);
-
+            //Insertamos todos los campos
             $insercion = User::create($this->request->all());
 
+            // Tratamiento de la imagen
             if (!empty($file)) {
 
+                // Creamos la carpeta de imagenes del usuario y la guardamos
                 $save = $file->move(public_path() . '/img/imgUser/' . $carpeta, $imagen);
 
+                // Redimensionamos la imagen del usuario
                 \Image::make(public_path() . '/img/imgUser/' . $carpeta . '/' . $imagen)->resize(200, 200)->save(public_path() . '/img/imgUser/' . $carpeta . '/' . $imagen);
 
             } else {
 
+                // Creamos la carpeta de imagenes del usuario
                 \File::makeDirectory(public_path() . '/img/imgUser/' . $carpeta);
+
+                // Redimensionamos la imagen por defecto del usuario y la guardamos en su carpeta
                 \Image::make(storage_path() . '/app/public/default/' . $imagen)->resize(200, 200)->save(public_path() . '/img/imgUser/' . $carpeta . '/' . $imagen);
 
             }
@@ -155,6 +152,7 @@ class UsersController extends Controller
         if(isset($insercion['id'])){
             return $insercion;
         }
+
         return false;
     } // create()
 
@@ -178,8 +176,10 @@ class UsersController extends Controller
         //indicamos que queremos guardar un nuevo archivo en el disco local
         $save = $file->move(public_path() . '/img/imgUser/' . \Auth::user()->carpeta, $nombre);
 
+        // Redimensionamos la imagen del usuario
         \Image::make(public_path() . '/img/imgUser/' . \Auth::user()->carpeta . '/' . $nombre)->resize(200, 200)->save(public_path() . '/img/imgUser/' . \Auth::user()->carpeta . '/' . $nombre);
 
+        // Si tengo la imagen guargo el nombre en la base de datos
         if ($save) {
             $user = new User;
             $user->where('id', '=', \Auth::user()->id)->update(['image' => $nombre]);
--- resources/views/partials/globals/uploadImage.blade.php
+++ resources/views/partials/globals/uploadImage.blade.php
@@ -2,28 +2,70 @@
 
 @section('content')
 @include('partials.nav.navEstudiante')
-	<div class="container">
+<div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Cambiar imagen</div>
+                <div class="modal-content">
+                    <div class="modal-header text-center">
 
-                <div class="panel-body">
-                    Bienvenido {{ Auth::user()->email }}
-                    {{ Auth::user()->image }}
+                        <h4><i class="fa fa-user"></i> Perfil de Usuario</h4>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <img src="{{ url('/img/imgUser/' . \Auth::user()->carpeta . '/' .  \Auth::user()->image) }}" alt="" class="img-responsive img-circle img-resize">
+
+                        </div>
+                    </div>
+                    
+                    {!! Form::open(['url' => 'profesor/UploadImg', 'method' => 'POST', 'files'=>'true', 'id' => 'my-dropzone' , 'class' => 'dropzone dropzone-width']) !!}
+                        <div class="dz-message">
+                            <h4><i class="fa fa-file-image-o" aria-hidden="true"></i></h4>
+                        </div>
+                        <button type="submit" class="btn btn-success" id="submit">Save</button>
+                    {!! Form::close() !!}
+                    <div><br></div>
                 </div>
-                <div style="margin: auto;">
-                	<img src="{{ url('/img/imgUser/' . \Auth::user()->carpeta . '/' .  \Auth::user()->image) }}" alt="" class="img-responsive img-circle img-resize">
-                </div>
-                <form method="POST" action="UploadImg" accept-charset="UTF-8" enctype="multipart/form-data">
-					<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-                	{{ Form::file('file',['id' => 'holi'])}}
-                	{{ Form::submit('Cambiar Imagen', ['class' => 'btn btn-primary'])}}
-                	
-                </form>
             </div>
         </div>
     </div>
 </div>
+
+@endsection
+
+@section('scripts')
+
+    <script>
+
+        Dropzone.options.myDropzone = {
+            autoProcessQueue: false,
+            uploadMultiple: false,
+            maxFilezise: 10,
+            maxFiles: 1,    
+            
+            init: function() {
+                var submitBtn = document.querySelector("#submit");
+                myDropzone = this;
+                
+                submitBtn.addEventListener("click", function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    myDropzone.processQueue();
+                });
+                
+                this.on("complete", function(file) {
+                    myDropzone.removeFile(file);
+                    location.reload();
+                });
+ 
+                this.on("success", 
+                    myDropzone.processQueue.bind(myDropzone)
+                );
+            }
+        };
+
+    </script>
 
 @endsection
