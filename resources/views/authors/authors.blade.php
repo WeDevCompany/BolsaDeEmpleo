7604cd74442543e189fb45468164092f9b23aeba
khru <evrtrabajo@gmail.com>
2016-04-30 20:41:09 +0200

    Página de autores

--- resources/views/authors/authors.blade.php
+++ resources/views/authors/authors.blade.php
@@ -7,12 +7,12 @@
     <div class="container navbar hidden"></div>
         <div class="row">
             <!--Cards section-->
-            <div class="container page-content container-card">
+            <div class="container page-content">
             <div class="container-fluid centrado">
-                <h3>¿Quienes Somos?</h3>
+                <h3>¿Quiénes Somos?</h3>
             </div>
                 <div class="col-md-4">
-                    <!--Rotating card-->
+                    <!--Rotating card - Card-01 -->
                     <div class="card-wrapper hoverable">
                         <div id="card-1" class="card-rotating effect__click">
 
@@ -65,39 +65,123 @@
                     <!--/.Rotating card-->
                 </div>
                 <div class="col-md-4">
-                        <div class="testimonial-card z-depth-1 hoverable">
-                            <a href="{{ url(config('routes.registro.registroEmpresa')) }}">
-                                <div class="mask waves-effect waves-light">
-                                    <div class="card-up g light-blue">
-                                    </div>
-                                    <div class="avatar"><img src="{{ url('/img/global/empresario_off.png')}}">
-                                    </div>
-                                    <div class="card-content">
-                                        <h5>Empresas</h5>
-                                        <p><i class="fa fa-quote-left"></i> Esta aplicación tiene como objetivo principal que las empresas dedicadas a todo tipo de negocios publiquen las ofertas de trabajo y/o FCT para obtener estudiantes altamente cualificados en distintas áreas, lo cual beneficiará tanto a la empresa como al alumno. <i class="fa fa-quote-right"></i></p>
-                                    </div>
+                    <!--Rotating card - Card-02 -->
+                    <div class="card-wrapper hoverable">
+                        <div id="card-2" class="card-rotating effect__click">
+
+                            <!--Front Side-->
+                            <div class="face card-rotating__front z-depth-1">
+                                <div class="card-up">
+                                    <img src="{{url("/img/authors/emmanuel-fondo.jpg")}}" class="img-responsive">
                                 </div>
-                            </a>
+                                <div class="avatar"><img src="{{url("/img/authors/emmanuel.jpg")}}" class="img-circle img-responsive">
+                                </div>
+                                <h4>Emmanuel Valverde Ramos</h4>
+                                <h5>Desarrollador Back-End</h5>
+
+                                <!--Triggering button-->
+                                <div class="text-center">
+                                    <a class="rotate-btn" data-card="card-2"><i class="fa fa-repeat"> Leer más</i></a>
+                                </div>
+
+                            </div>
+                            <!--/.Front Side-->
+
+                            <!--Back Side-->
+                            <div class="face card-rotating__back z-depth-1">
+                                <h4>Emmanuel Valverde Ramos</h4>
+                                <p class="card-content text-center">Jefe de proyecto
+                                    <br> Conocimientos en:</p>
+                                <hr> PHP (PDO, MVC, Laravel,...)
+                                <br> MySQl
+                                <br> Java
+                                <br> C
+                                <br> Jquery
+                                <br> CSS
+                                <br>
+
+                                <!--Triggering button-->
+                                <a class="rotate-btn" data-card="card-2"><i class="fa fa-undo"> Volver</i></a>
+                                <div class="sm-container">
+                                    <ul class="list-inline card-sm">
+                                        <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="icons-sm tw-ic" href="https://twitter.com/evrtrabajo" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
+                                        <li><a class="icons-sm li-ic"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!--/.Back Side-->
+
                         </div>
+                    </div>
+                    <!--/.Rotating card-->
                 </div>
                 <div class="col-md-4">
-                        <div class="testimonial-card z-depth-1 hoverable">
-                            <a href="{{ url(config('routes.registro.registroProfesor')) }}">
-                                <div class="mask waves-effect waves-light">
-                                    <div class="card-up yellow">
-                                    </div>
-                                    <div class="avatar"><img src="{{ url('/img/global/profesor.jpg')}}" class="img-circle img-responsive">
-                                    </div>
-                                    <div class="card-content">
-                                        <h5>Profesores</h5>
-                                        <p><i class="fa fa-quote-left"></i> En esta web los profesores podrán gestionar toda la información sobre ofertas de trabajo y/o prácticas para sus estudiantes ya sean de su rama o de otra, permitiendo que sean los mismos estudiantes quienes se suscriban dichas ofertas, facilitando esto su labor de selección de empresas. <i class="fa fa-quote-right"></i></p>
-                                    </div>
+                    <!--Rotating card - Card-03 -->
+                    <div class="card-wrapper hoverable">
+                        <div id="card-3" class="card-rotating effect__click">
+
+                            <!--Front Side-->
+                            <div class="face card-rotating__front z-depth-1">
+                                <div class="card-up">
+                                    <img src="{{url("/img/authors/emmanuel-fondo.jpg")}}" class="img-responsive">
                                 </div>
-                            </a>
+                                <div class="avatar"><img src="{{url("/img/authors/emmanuel.jpg")}}" class="img-circle img-responsive">
+                                </div>
+                                <h4>Emmanuel Valverde Ramos</h4>
+                                <h5>Desarrollador Back-End</h5>
+
+                                <!--Triggering button-->
+                                <div class="text-center">
+                                    <a class="rotate-btn" data-card="card-3"><i class="fa fa-repeat"> Leer más</i></a>
+                                </div>
+
+                            </div>
+                            <!--/.Front Side-->
+
+                            <!--Back Side-->
+                            <div class="face card-rotating__back z-depth-1">
+                                <h4>Emmanuel Valverde Ramos</h4>
+                                <p class="card-content text-center">Jefe de proyecto
+                                    <br> Conocimientos en:</p>
+                                <hr> PHP (PDO, MVC, Laravel,...)
+                                <br> MySQl
+                                <br> Java
+                                <br> C
+                                <br> Jquery
+                                <br> CSS
+                                <br>
+
+                                <!--Triggering button-->
+                                <a class="rotate-btn" data-card="card-3"><i class="fa fa-undo"> Volver</i></a>
+                                <div class="sm-container">
+                                    <ul class="list-inline card-sm">
+                                        <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a class="icons-sm tw-ic" href="https://twitter.com/evrtrabajo" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
+                                        <li><a class="icons-sm li-ic"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!--/.Back Side-->
+
                         </div>
+                    </div>
+                    <!--/.Rotating card-->
                 </div>
             </div>
             <!-- /.Cards -->
+            <div class="text-center">
+                <a href="javascript:window.history.back();" class="btn btn-primary btn-login-media  waves-effect waves-light" id="volver">
+                    <div class="show-responsive">
+                        <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                    </div>
+                    <div class="hidden-media">
+                        <i class="fa fa-arrow-left" aria-hidden="true"></i> <span class="hidden-media">Volver</span>
+                    </div>
+                </a>
+            </div>
 
         </div>
     </div>
