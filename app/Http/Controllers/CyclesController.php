36068a2fe335cddeb2592df2507a4d788a10e3a9
khru <evrtrabajo@gmail.com>
2016-05-01 12:59:56 +0200

    Modificación de rutas y métodos con ajax

--- app/Http/Controllers/CyclesController.php
+++ app/Http/Controllers/CyclesController.php
@@ -4,9 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+// Utilizamos las siguientes clases
+use App\Cycle;
 
 class CyclesController extends Controller
 {
-    //
+    // Variable con la petición realizada
+    private $request;
+
+    /**
+     * Método constructor
+     * @param Request $request Request recibido de las rutas
+     */
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+
+    /**
+     * Método que obtiene los ciclos por Ajax y devuelve los resultados
+     * en formato JSON
+     * @param  Integer $familyId ID de la familia profesional
+     * @return JSON | abort      JSON con la información de la consulta
+     *                           Abort en caso de que el ID no sea valido
+     */
+    public function getCiclesJSON($familyId){
+        // Tratamos el ID
+        $familyId = (int) $familyId;
+        // coprobamos que el id es valido
+        if(is_numeric($familyId) && $familyId > 0){
+            $cycles = Cycle::where('active', '=', '1')->where('profFamilie_id', '=', $familyId)->get();
+            // devolvemos el resultado de la consulta en formato JSON
+            return \Response::json($cycles);
+        }
+
+        // Si el id de la familia profesional ha sido modificado
+        abort('404');
+    }// getCiclesJSON()
 }
--- app/Http/Controllers/ProfFamilieController.php
+++ app/Http/Controllers/ProfFamilieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\ProfFamilie;
 use App\Http\Requests;
+use Response;
 
 class ProfFamilieController extends Controller
 {
@@ -17,10 +18,12 @@ class ProfFamilieController extends Controller
     	try {
             // Añado la opcion por defecto
             $profFamilies = ['0' => 'Selecciona una familia profesional'];
-    		
-            // Obtengo las familias activas 
+
+            // Obtengo las familias activas
         	$profFamiliesDB = ProfFamilie::where('active', '=', 1)->lists('name', 'id')->toArray();
 
+			// Le "añadimos" a las familias profesionales una hecha
+			// por nosotros
             $profFamilies = $profFamilies + $profFamiliesDB;
 
     	} catch(\PDOException $e) {
@@ -28,5 +31,32 @@ class ProfFamilieController extends Controller
             abort(500);
     	}
     	return $profFamilies;
-    }
+    }// getAllProfFamilies()
+
+	/**
+	 * Método que genera las familias profesionales por Ajax respondiendo JSON
+	 * [En la ruta no se debe utilizar un namespace JSON este archivo no está
+	 * en la carpeta Json]
+	 * @return JSON devuelve el resultado de la consulta en formato JSON
+	 */
+	public function getAllProfFamiliesJSON(){
+		// Bloque try catch en el que realizaremos la consulta
+		// de todas las familias profecionales activas
+		// y las devolveremos en formato JSON
+		try{
+			// resultados de la consulta
+			$profFamilies = ProfFamilie::where('active', '=', '1')->lists('name', 'id');
+
+			// Formamos un array con las familias validas
+	        $valid_profFamilies = [];
+	        foreach ($profFamilies as $id => $profFamilie) {
+	            $valid_profFamilies[] = ['id' => $id, 'familia' => $profFamilie];
+	        }
+	        return \Response::json($valid_profFamilies);
+		} catch (\PDOException $e){
+			//dd($e);
+            abort(500);
+		}
+
+	}
 }
