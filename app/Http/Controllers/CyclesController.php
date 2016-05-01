d4d9a09b6b4c3c6bbf5718814b2a1b23a936fbd3
khru <evrtrabajo@gmail.com>
2016-05-01 14:05:44 +0200

    Optimización de la seguridad de los ciclos.

--- app/Http/Controllers/CyclesController.php
+++ app/Http/Controllers/CyclesController.php
@@ -23,25 +23,30 @@ class CyclesController extends Controller
     /**
      * Método que obtiene los ciclos por Ajax y devuelve los resultados
      * en formato JSON
-     * @param  Integer $familyId ID de la familia profesional
+     * @param  Integer $familyId ID de la familia profesional, si no se recibe
+     *                           por defecto es 0
      * @return JSON | abort      JSON con la información de la consulta
      *                           Abort en caso de que el ID no sea valido
      */
-    public function getCiclesJSON($familyId){
+    public function getCiclesJSON($familyId = 0){
         // Tratamos el ID
         $familyId = (int) $familyId;
         // coprobamos que el id es valido
         if(is_numeric($familyId) && $familyId > 0){
-            // Añadimos a la caché los resultados de los ciclos
-			// la caché dura 24 horas o 1440 minutos
-			// La unica forma de pasar como parametro a esta función anonima
-			// una variable es [use ($variable)]
-			$cycles = \Cache::remember('cycles', 1440, function() use ($familyId){
-				// Los resultados de la consulta se almacenan en la variable
-			    return Cycle::where('active', '=', '1')->where('profFamilie_id', '=', $familyId)->get();
+            try{
+                // Añadimos a la caché los resultados de los ciclos
+    			// la caché dura 24 horas o 1440 minutos
+    			// La unica forma de pasar como parametro a esta función anonima
+    			// una variable es [use ($variable)]
+    			$cycles = \Cache::remember('cycles', 1440, function() use ($familyId){
+    				// Los resultados de la consulta se almacenan en la variable
+    			    return Cycle::where('active', '=', '1')->where('profFamilie_id', '=', $familyId)->get();
 
-		    });
-            
+    		    });
+            } catch(\PDOException $e) {
+                abort('500');
+            }
+
             // devolvemos el resultado de la consulta en formato JSON
             return \Response::json($cycles);
         }
@@ -50,4 +55,4 @@ class CyclesController extends Controller
         abort('404');
     }// getCiclesJSON()
 
-}
+}// final del controlador de ciclos
