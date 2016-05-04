a35426915abb513218336ba2cdde2ff7dfd8a6d4
khru <evrtrabajo@gmail.com>
2016-05-04 17:44:03 +0200

    Clase de funciones un poco más comentada

--- public/js/funcionalidad/funciones.js
+++ public/js/funcionalidad/funciones.js
@@ -4,11 +4,17 @@
  */
 
     var funciones = {
-
-        generarSelectYears: function(id, end) {
+        /**
+         * Función que generá un campo select como string y lo devuelve
+         *
+         * @param  String id        ID que queremos que tenga nuestro select
+         * @param  Integer start    Desde que año quieres que empiece
+         * @return String  select   Select ya formado
+         */
+        generarSelectYears: function(id, start) {
             var anyo = (new Date).getFullYear();
         	var select = '<select class="form-control" name="' + id + '" id="' + id + '">';
-        	for (k = end; k <= anyo; k++)
+        	for (k = start; k <= anyo; k++)
             {
                 select += '<option value="' + k + '">' + k + '</option>';
             }
