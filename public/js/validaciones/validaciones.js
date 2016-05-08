160cc4dbba52bc94f3f2544d4b9869db93b34362
khru <evrtrabajo@gmail.com>
2016-05-08 12:49:10 +0200

    Quitadas las lineas inecesareas de las validaciones

--- public/js/validaciones/validaciones.js
+++ public/js/validaciones/validaciones.js
@@ -64,11 +64,9 @@
 
                     var regex = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
                         /*var regexObjce = new RegExp(nameRegex);*/
-                        console.log(regex.test(objectVal));
                         return regex.test(objectVal);
                 case 'regexPass':
                         var regex = "/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z-_$@ñÑáéíóúÁÉÍÓÚÀÈÌÒÙäëïöüÄËÏÖÜ]{6,20}$/";
-                        console.log(regex.test(objectVal));
                         return regex.test(objectVal);
                 default:
                     return false;
@@ -182,15 +180,9 @@
         objectValid : function (object, id, error, campo, funcionRegex){
             // comproamos si el email es valido
             var callbacks = null;
-            callbacks = $.Callbacks('stopOnFalse');
-            callbacks.empty();
-            callbacks.add( funcionRegex );
-            console.log(callbacks.length);
-            //if (!callbacks.fire(object.val())) {
             if (!this.funcionRegex(funcionRegex,object.val())) {
                return this.errorObject(object, id, error, campo);
             }
-            console.log('test');
             return true;
         },
 
