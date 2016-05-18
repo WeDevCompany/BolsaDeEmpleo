02c2e4a82e37763470b0bb00d9fb1f27b504dbb7
khru <evrtrabajo@gmail.com>
2016-05-18 21:08:01 +0200

    Rutas del menú admin añadidas

--- resources/views/partials/nav/navAdministrador.blade.php
+++ resources/views/partials/nav/navAdministrador.blade.php
@@ -10,8 +10,8 @@
         <!-- ADMIN -->
         <li role="presentation" class="dropdown-header">Administrador</li>
         <li><a href="{{ url(config('routes.admin.teacherNotification')) }}"><i class="fa fa-university right" aria-hidden="true"></i><span class="badge social-counter right notificacion">5</span> Profesores</a></li>
-        <li><a href="{{ url(config('routes.teacher.studentNotification')) }}"><i class="fa fa-graduation-cap right"  aria-hidden="true"></i><span class="badge social-counter right notificacion">5</span> Alumnos</a></li>
-        <li><a href="{{ url(config('routes.teacher.offertNotification')) }}"><i class="fa fa-cart-plus right"  aria-hidden="true"></i><span class="badge social-counter right notificacion">5</span> Ofertas</a></li>
+        <li><a href="{{ url(config('routes.admin.studentNotification')) }}"><i class="fa fa-graduation-cap right"  aria-hidden="true"></i><span class="badge social-counter right notificacion">88</span> Alumnos</a></li>
+        <li><a href="{{ url(config('routes.admin.offerNotification')) }}"><i class="fa fa-cart-plus right"  aria-hidden="true"></i><span class="badge social-counter right notificacion">5</span> Ofertas</a></li>
         <!-- PROFESORES -->
         <li role="presentation" class="dropdown-header">Profesor</li>
         <li><a href="{{ url(config('routes.teacher.studentNotification')) }}"><i class="fa fa-graduation-cap right"  aria-hidden="true"></i><span class="badge social-counter right notificacion">5</span> Alumnos</a></li>
@@ -41,13 +41,13 @@
     <ul class="dropdown-menu" role="menu" aria-labelledby="ofertas">
         <!-- ADMIN -->
         <li role="presentation" class="dropdown-header">Administrador</li>
-        <li><a href="{{ url(config('routes.admin.allVerifiedTeachers')) }}"><i class="fa fa-check right" aria-hidden="true"></i> Admitidas</a></li>
-        <li><a href="{{ url(config('routes.admin.allDeniedTeachers')) }}"><i class="fa fa-times right" aria-hidden="true"></i> Denegadas</a></li>
+        <li><a href="{{ url(config('routes.admin.allVerifiedOffers')) }}"><i class="fa fa-check right" aria-hidden="true"></i> Admitidas</a></li>
+        <li><a href="{{ url(config('routes.admin.allDeniedOffers')) }}"><i class="fa fa-times right" aria-hidden="true"></i> Denegadas</a></li>
 
         <!-- PROFESORES -->
         <li role="presentation" class="dropdown-header">Profesor</li>
         <li><a href="{{ url(config('routes.teacher.allVerifiedOffers')) }}"><i class="fa fa-check right" aria-hidden="true"></i> Admitidas</a></li>
-        <li><a href="{{ url(config('routes.registro.registroProfesor')) }}"><i class="fa fa-times right" aria-hidden="true"></i> Denegadas</a></li>
+        <li><a href="{{ url(config('routes.teacher.allDeniedOffers')) }}"><i class="fa fa-times right" aria-hidden="true"></i> Denegadas</a></li>
     </ul>
 </a></li>
 <!-- ADMIN -->
