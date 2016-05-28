cd6fa8f363f1001713754d2fbefaf03a54d4a133
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2016-05-28 21:39:06 +0200

    Añadida la zona y los filtros

---
+++ config/zona.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+	'notificaciones' => [
+		'estudiante' => 'Notificaciones de estudiantes',
+		'profesor' => 'Notificaciones de profesores',
+		'empresa' => 'Notificaciones de ofertas'
+	],
+	'admitidos' => [
+		'estudiante' => 'Estudiantes admitidos',
+		'profesor' => 'Profesores admitidos',
+		'empresa' => 'Ofertas admitidas'
+	],
+	'denegados' => [
+		'estudiante' => 'Estudiantes denegados',
+		'profesor' => 'Profesores denegados',
+		'empresa' => 'Ofertas denegadas'
+	],
+	'configuracion' => [
+		'profFamilies' => 'Familias profesionales',
+		'cycles' => 'Ciclos',
+		'subjects' => 'Asignaturas',
+		'tags' => 'Etiquetas'
+	],
+	'estadisticas' => [
+
+	]
+];
+
+?>
