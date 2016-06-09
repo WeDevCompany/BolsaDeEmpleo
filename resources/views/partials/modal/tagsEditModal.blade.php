e114bd859a9aff470eb542942a58e8f60befa265
Eduardo Lopez <edu230895@gmail.com>
2016-06-09 19:26:02 +0200

    Parte de Tags inacabada

---
+++ resources/views/partials/modal/tagsEditModal.blade.php
@@ -0,0 +1,48 @@
+<!-- Modal -->
+<div class="modal fade" id="myModal{{ $id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        {{ Form::open(['url' => 'profesor/tags/' . $id, 'method' => 'POST', 'id' => 'tag-form']) }}
+            {!! csrf_field() !!}
+            <!--Content-->
+            <div class="modal-content border-orange">
+                <!--Header-->
+                <div class="modal-header text-center no-padding-bottom">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title titleTag" id="myModalLabel">Editar Tags</h4>
+                </div>
+                <!--Body-->
+                <div class="modal-body">
+                    <h5 class="text-center">{{ $subject }}</h5>
+                    @include('tag.partials.tagsFields')
+                </div>
+                <!--Footer-->
+                <div class="modal-footer">
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-secondary waves-effect waves-light pull-left" data-dismiss="modal">
+                            <div class="show-responsive">
+                                <i class="fa fa-times" aria-hidden="true"></i>
+                            </div>
+                            <div class="hidden-media">
+                                <i class="fa fa-times"></i> <span class="hidden-media">Cerrar</span>
+                            </div>
+                        </button>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-warning waves-effect waves-light">
+                            <div class="show-responsive">
+                                <i class="fa fa-btn fa-user" aria-hidden="true"></i>
+                            </div>
+                            <div class="hidden-media">
+                                <i class="fa fa-btn fa-user"></i> <span class="hidden-media">Editar</span>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!--/.Content-->
+        {{ Form::close() }}
+    </div>
+</div>
+<!-- /.Live preview-->
---
+++ resources/views/tag/partials/tagsFields.blade.php
@@ -0,0 +1,7 @@
+<div class="control-group">
+    <div class="row">
+        <div class="input-field col-md-12">
+            {{ Form::textarea('body', null,['id' => "body[$id]", 'class' => 'border-blue textAreaTags']) }}
+        </div>
+    </div>
+</div>
