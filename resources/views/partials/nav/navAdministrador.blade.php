f9ec2cb6f35d2099e1aa7713495aad5359937d9b
Sikay <pedrohdezmora@gmail.com>
2016-06-11 19:58:55 +0200

    Correccion de errores

--- resources/views/partials/nav/navAdministrador.blade.php
+++ resources/views/partials/nav/navAdministrador.blade.php
@@ -15,7 +15,7 @@
         <!-- PROFESORES -->
         <li role="presentation" class="dropdown-header">Profesor</li>
         <li><a href="{{ url(config('routes.teacher.studentNotification')) }}"><i class="fa fa-graduation-cap right"  aria-hidden="true"></i><span class="badge social-counter right notificacion" id="studentNotifications"></span> Alumnos</a></li>
-        <li><a href="{{ url(config('routes.teacher.offertNotification')) }}"><i class="fa fa-cart-plus right"  aria-hidden="true"></i><span class="badge social-counter right notificacion" id="offerNotifications"></span> Ofertas</a></li>
+        <li><a href="{{ url(config('routes.teacher.offerNotification')) }}"><i class="fa fa-cart-plus right"  aria-hidden="true"></i><span class="badge social-counter right notificacion" id="offerNotifications"></span> Ofertas</a></li>
     </ul>
 </a></li>
 <li class = "dropdown">
