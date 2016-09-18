49d5423ff0ead2863eb5a6587a784e5df32f83ac
khru <evrtrabajo@gmail.com>
2016-09-18 20:37:36 +0200

    Autores modificación

--- resources/views/authors/authors.blade.php
+++ resources/views/authors/authors.blade.php
@@ -16,63 +16,8 @@
                     <div class="panel-body">
                         <div class="row">
                             <div class="container page-content col-md-12">
-                                <div class="col-md-4">
-                                    <!--Rotating card - Card-02 -->
-                                    <div class="card-wrapper hoverable">
-                                        <div id="card-2" class="card-rotating effect__click">
 
-                                            <!--Front Side-->
-                                            <div class="face card-rotating__front z-depth-1">
-                                                <div class="card-up">
-                                                    <img src="{{url("/img/authors/eduardo-fondo.jpg")}}" class="img-responsive">
-                                                </div>
-                                                <div class="avatar"><img src="{{url("/img/authors/eduardo.jpg")}}" class="img-circle img-responsive">
-                                                </div>
-                                                <h4>Eduardo López <br>Pardo</h4>
-                                                <h5>Desarrollador Back-End</h5>
-                                                
-
-                                                <!--Triggering button-->
-                                                <div class="text-center">
-                                                    <a class="rotate-btn btn btn-primary"  data-card="card-2">Leer más</a>
-                                                </div>
-
-                                            </div>
-                                            <!--/.Front Side-->
-
-                                            <!--Back Side-->
-                                            <div class="face card-rotating__back z-depth-1">
-                                                <h4>Eduardo López Pardo</h4>
-                                                <p class="card-content text-center">
-                                                    <br>Experiencia con:</p>
-                                                <hr> PHP [POO,PDO,MVC,Laravel]
-                                                <br> MySQl [Correlacionadas,Normalización]
-                                                <br> Ajax [Asincronía]
-                                                <br> Jquery
-                                                <br> Java
-                                                <br> CSS [Bootstrap]
-                                                <br> HTML
-                                                <br>
-
-                                                <!--Triggering button-->
-                                                <a class="rotate-btn btn btn-primary" data-card="card-2">Volver</a>
-                                                <div class="sm-container">
-                                                    <ul class="list-inline card-sm">
-                                                        <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
-                                                        <li><a class="icons-sm tw-ic" href="https://twitter.com/DuardoCris23" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                                                        <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
-                                                        <li><a class="icons-sm li-ic" href="https://www.linkedin.com/in/eduardo-l%C3%B3pez-pardo-20964111b"><i class="fa fa-linkedin"></i></a></li>
-                                                        <li><a class="icons-sm git-ic" href="https://github.com/Quorum23"><i class="fa fa-github" target="_blank"> </i></a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <!--/.Back Side-->
-
-                                        </div>
-                                    </div>
-                                    <!--/.Rotating card-->
-                                </div>
-                                <div class="col-md-4">
+                                <div class="col-md-6">
                                     <!--Rotating card - Card-01 -->
                                     <div class="card-wrapper hoverable">
                                         <div id="card-1" class="card-rotating effect__click">
@@ -126,7 +71,7 @@
                                     </div>
                                     <!--/.Rotating card-->
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-md-6">
                                     <!--Rotating card - Card-03 -->
                                     <div class="card-wrapper hoverable">
                                         <div id="card-3" class="card-rotating effect__click">
@@ -182,7 +127,10 @@
                             </div>
                         <!-- /.Cards -->
                         <div class="text-center extra-padding">
-                            <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">bolsa de empleo</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="" property="cc:attributionName" rel="cc:attributionURL"><b>Emmanuel Valverde Ramos</b>, <b>Pedro Hernández-Mora de fuentes</b>, <b>Eduardo López Pardo</b></a> is licensed under a<br /><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Reconocimiento-NoComercial-SinObraDerivada 4.0 Internacional License</a>.
+                            <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">bolsa de empleo</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="" property="cc:attributionName" rel="cc:attributionURL"><b>Emmanuel Valverde Ramos</b>, <b>Pedro Hernández-Mora de fuentes</b></a> is licensed under a<br /><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Reconocimiento-NoComercial-SinObraDerivada 4.0 Internacional License</a>.
+                        </div>
+                        <div class="text-center hidden">
+                            Colaboradores: Eduardo López Pardo
                         </div>
                         <div class="text-center">
                             <a href="javascript:window.history.back();" class="btn btn-primary btn-login-media  waves-effect waves-light" id="volver">
